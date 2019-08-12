c57f00e932db7f1599b77297883f905b912e8742
lianan gouoo <liang164458913@gmail.com>
2019-08-12 15:01:45 +0800

    update espnow library

--- espNow/espnow.cpp
+++ espNow/espnow.cpp
@@ -1,96 +1,12 @@
 #include "espnow.h"
 
 EspNow::EspNow(void) {
-  device_name = (char*)malloc(sizeof(char) * 30);
-  setDeviceName("Slave");
+  // device_name = (char*)malloc(sizeof(char) * 30);
+  // setDeviceName("Slave");
 }
 
 EspNow::~EspNow(void) {
-  free(device_name);
-}
-
-void EspNow::InitESPNow(void) {
-  WiFi.mode(WIFI_STA);
-  //Serial.print("AP MAC: ");Serial.println(WiFi.softAPmacAddress());
-
-  WiFi.disconnect();
-  if (esp_now_init() == ESP_OK) {
-    Serial.println("ESPNow Init Success");
-  }
-  else {
-    Serial.println("ESPNow Init Failed");
-    // Retry InitESPNow, add a counte and then restart?
-    // InitESPNow();
-    // or Simply Restart
-    ESP.restart();
-  }
-
-  esp_now_register_send_cb(OnDataSent);
-  ScanForSlave();
-}
-
-// Scan for slaves in AP mode
-void EspNow::ScanForSlave(void) {
-  int8_t scanResults = WiFi.scanNetworks();
-  // reset on each scan
-  bool slaveFound = 0;
-  memset(&slave, 0, sizeof(slave));
-
-  Serial.println("");
-  if (scanResults == 0) {
-    Serial.println("No WiFi devices in AP Mode found");
-  } else {
-//    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
-    for (int i = 0; i < scanResults; ++i) {
-      // Print SSID and RSSI for each device found
-      String SSID = WiFi.SSID(i);
-      int32_t RSSI = WiFi.RSSI(i);
-      String BSSIDstr = WiFi.BSSIDstr(i);
-
-      if (PRINTSCANRESULTS) {
-        Serial.print(i + 1);
-        Serial.print(": ");
-        Serial.print(SSID);
-        Serial.print(" (");
-        Serial.print(RSSI);
-        Serial.print(")");
-        Serial.println("");
-      }
-      delay(10);
-      // Check if the current device starts with `Slave`
-      if (SSID.indexOf(device_name) == 0) {
-        // SSID of interest
-        
-		    Serial.println("Found a Slave.");
-        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
-        // Get BSSID => Mac Address of the Slave
-        
-		int mac[6];
-        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x%c",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
-          for (int ii = 0; ii < 6; ++ii ) {
-            slave.peer_addr[ii] = (uint8_t) mac[ii];
-          }
-        }
-
-        slave.channel = CHANNEL; // pick a channel
-        slave.encrypt = 0; // no encryption
-
-        slaveFound = 1;
-        // we are planning to have only one slave in this example;
-        // Hence, break after we find one, to be a bit efficient
-        break;
-      }
-    }
-  }
-
-  if (slaveFound) {
-    //Serial.println("Slave Found, processing..");
-  } else {
-    Serial.println("Slave Not Found, trying again.");
-  }
-
-  // clean up ram
-  WiFi.scanDelete();
+  // free(device_name);
 }
 
 // Check if the slave is already paired with the master.
@@ -195,4 +111,151 @@ void EspNow::OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.print("Last Packet Sent to: "); Serial.println(macStr);
   Serial.print("Last Packet Send Status: "); Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
+
+void EspNow::OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  char macStr[18];
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print("Last Packet Recv from: "); Serial.println(macStr);
+  Serial.print("Last Packet Recv Data: "); Serial.println(*data);
+  Serial.println("");
+}
+
+EspNowMaster::EspNowMaster() {
+  device_name = (char*)malloc(sizeof(char) * 30);
+  setDeviceName("Slave");
+}
+
+EspNowMaster::~EspNowMaster() {
+  free(device_name);
+}
+
+// Scan for slaves in AP mode
+void EspNowMaster::ScanForSlave(void) {
+  int8_t scanResults = WiFi.scanNetworks();
+  // reset on each scan
+  bool slaveFound = 0;
+  memset(&slave, 0, sizeof(slave));
+
+  Serial.println("");
+  if (scanResults == 0) {
+    Serial.println("No WiFi devices in AP Mode found");
+  } else {
+//    Serial.print("Found "); Serial.print(scanResults); Serial.println(" devices ");
+    for (int i = 0; i < scanResults; ++i) {
+      // Print SSID and RSSI for each device found
+      String SSID = WiFi.SSID(i);
+      int32_t RSSI = WiFi.RSSI(i);
+      String BSSIDstr = WiFi.BSSIDstr(i);
+
+      if (PRINTSCANRESULTS) {
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(SSID);
+        Serial.print(" (");
+        Serial.print(RSSI);
+        Serial.print(")");
+        Serial.println("");
+      }
+      delay(10);
+      // Check if the current device starts with `Slave`
+      if (SSID.indexOf(device_name) == 0) {
+        // SSID of interest
+        
+		    Serial.println("Found a Slave.");
+        Serial.print(i + 1); Serial.print(": "); Serial.print(SSID); Serial.print(" ["); Serial.print(BSSIDstr); Serial.print("]"); Serial.print(" ("); Serial.print(RSSI); Serial.print(")"); Serial.println("");
+        // Get BSSID => Mac Address of the Slave
+        
+		int mac[6];
+        if ( 6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x%c",  &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5] ) ) {
+          for (int ii = 0; ii < 6; ++ii ) {
+            slave.peer_addr[ii] = (uint8_t) mac[ii];
+          }
+        }
+        slave.channel = CHANNEL; // pick a channel
+        slave.encrypt = 0; // no encryption
+
+        slaveFound = 1;
+        // we are planning to have only one slave in this example;
+        // Hence, break after we find one, to be a bit efficient
+        break;
+      }
+    }
+  }
+
+  if (slaveFound) {
+    //Serial.println("Slave Found, processing..");
+  } else {
+    Serial.println("Slave Not Found, trying again.");
+  }
+
+  // clean up ram
+  WiFi.scanDelete();
+}
+
+void EspNowMaster::InitESPNow(void) {
+
+  WiFi.mode(WIFI_STA);
+  //Serial.print("AP MAC: ");Serial.println(WiFi.softAPmacAddress());
+
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+
+    esp_now_register_send_cb(OnDataSent);
+    // esp_now_register_recv_cb(OnDataRecv);
+  ScanForSlave();
+}
+
+void EspNowMaster::setRecvCallBack(void (*cb)(const unsigned char *, const unsigned char *, int)){
+  esp_now_register_recv_cb(cb);
+}
+
+void EspNowMaster::setSendCallBack(void (*cb)(const unsigned char *, esp_now_send_status_t)){
+  esp_now_register_send_cb(cb);
+}
+
+EspNowSlave::EspNowSlave() {
+  // device_name = (char*)malloc(sizeof(char) * 30);
+  // setDeviceName("Slave");
+}
+
+EspNowSlave::~EspNowSlave() {
+  // free(device_name);
+}
+
+void EspNowSlave::InitESPNow(void) {
+
+  const char *SSID = "Slave_1";
+  bool result = WiFi.softAP(SSID, "Slave_1_Password", CHANNEL, 0);
+  if (!result) {
+    Serial.println("AP Config failed.");
+  } else {
+    Serial.println("AP Config Success. Broadcasting with AP: " + String(SSID));
+  }
+  WiFi.mode(WIFI_AP_STA);
+  //Serial.print("AP MAC: ");Serial.println(WiFi.softAPmacAddress());
+
+  WiFi.disconnect();
+  if (esp_now_init() == ESP_OK) {
+    Serial.println("ESPNow Init Success");
+  }
+  else {
+    Serial.println("ESPNow Init Failed");
+    // Retry InitESPNow, add a counte and then restart?
+    // InitESPNow();
+    // or Simply Restart
+    ESP.restart();
+  }
+
+  esp_now_register_send_cb(&OnDataSent);
 }
