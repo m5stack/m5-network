ec4db4e99c42599a9c9b6ff99a260e188b8d8310
lianan gouoo <liang164458913@gmail.com>
2019-08-14 20:38:36 +0800

    change the whole class

--- espNow/espnow.h
+++ espNow/espnow.h
@@ -1,60 +1,79 @@
 #ifndef __ESPNOW_H
 #define __ESPNOW_H
 
-#include <Preferences.h>
 #include <esp_now.h>
+#include <Preferences.h>
 #include <WiFi.h>
 #include <String>
 
-//#define CHANNEL 1
 
+#define SLAVE_CHANNEL 1
 #define CHANNEL 3
 #define PRINTSCANRESULTS 0
 #define DELETEBEFOREPAIR 0
 
-class EspNow {
+typedef void (*recvCB)(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+typedef void (*sendCB)(const uint8_t *mac_addr, esp_now_send_status_t status);
 
+class EspNow {
 public:
-	esp_now_peer_info_t slave;
-	char* device_name = NULL;
+	static esp_now_peer_info_t slave;
 
 	EspNow();
 	~EspNow();
-	// void setDeviceName(char* name) { sprintf(device_name, name); };
-	// void InitESPNow(void);
-	void deletePeer(void); 
-	void sendData(void *buf, int len);
-	bool manageSlave(void);
+	static void sendData(void *buff, int len);
 	void setRecvCallBack(void (*cb)(const unsigned char *, const unsigned char *, int));
-	void setSendCallBack(void (*cb)(const unsigned char *, esp_now_send_status_t));
-	static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
-	static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+    void setSendCallBack(void (*cb)(const unsigned char *, esp_now_send_status_t));
+	void deletePeer(void);
+	bool manageSlave(void);
 };
 
-class EspNowMaster :public EspNow {
+struct peerList {
+	esp_now_peer_info_t list[40];
+	uint8_t count = 0;
+};
+
+class EspNowMaster : public EspNow {
 public:
-	bool onRecvEnable = false;
-	bool onSentEnable = false;
+	bool isConnected = false;
+	static peerList peerlist;
+	static recvCB recvCallBack;
+	static sendCB sendCallBack;
 
 	EspNowMaster();
 	~EspNowMaster();
-	void ScanForSlave(void);
-	void setDeviceName(char* name) { sprintf(device_name, name); };
-	void InitESPNow(void);
+	static int8_t isPeerExist(esp_now_peer_info_t peer_info);
+	static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+	static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+	void InitBroadcastSlave(void);
+	bool confirmPeer(esp_now_peer_info_t peer);
+	void Broadcast();
+	void Init();
 
 private:
-	Preferences preferences; 
+	Preferences preferences;
 };
 
-class EspNowSlave :public EspNow {
+class EspNowSlave : public EspNow {
 public:
-	bool onRecvEnable = false;
-	bool onSentEnable = false;
+	static peerList peerlist;
+	// static void (*recvCallBack)(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+	static recvCB recvCallBack;
+	static sendCB sendCallBack;
 
 	EspNowSlave();
 	~EspNowSlave();
-	void setDeviceName(char* name) { sprintf(device_name, name); };
-	void InitESPNow(void);
+	void Ack(esp_now_peer_info_t peer);
+	void Init();
+	void configDeviceAP();
+	static int8_t isPeerExist(esp_now_peer_info_t peer);
+	static void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status);
+	static void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len);
+	void setRecvCallBack(void (*cb)(const uint8_t *, const uint8_t *, int)) {recvCallBack = cb;};
+	void setSendCallBack(void (*cb)(const uint8_t *, esp_now_send_status_t));
 
+private:
+	Preferences preferences;
 };
+
 #endif
