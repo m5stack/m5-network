1bfbd78dfe86991c5bb635bf813ebf0476e23b45
lianan gouoo <liang164458913@gmail.com>
2019-08-15 15:21:43 +0800

    little change for slave example

--- espNow/examples/espNowSlave/espNowSlave.ino
+++ espNow/examples/espNowSlave/espNowSlave.ino
@@ -4,11 +4,13 @@
 EspNowSlave espnow;
 
 void setLcd() {
+  M5.Lcd.setCursor(0, 0);
   M5.Lcd.clear(WHITE);
   M5.Lcd.setTextColor(RED);
   M5.Lcd.setBrightness(200);
   M5.Lcd.setTextSize(2);
-  M5.Lcd.print("espnow slave test");
+  M5.Lcd.println("espnow slave test");
+  M5.Lcd.print(WiFi.softAPmacAddress());
 }
 
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
@@ -28,19 +30,39 @@ void setup() {
   setLcd();
   espnow.Init();
   espnow.setRecvCallBack(OnDataRecv);
-  M5.Lcd.println();
-  M5.Lcd.print(WiFi.softAPmacAddress());
 }
 
+void printPeerList() {
+  setLcd();
+  M5.Lcd.setCursor(0, 50);
+  M5.Lcd.printf("who is broadcasting :\r\n");
+  for(int i = 0; i < espnow.peerlist.count; i++) {
+    char macStr[18];
+    snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           espnow.peerlist.list[i].peer_addr[0], 
+           espnow.peerlist.list[i].peer_addr[1],
+           espnow.peerlist.list[i].peer_addr[2],
+           espnow.peerlist.list[i].peer_addr[3],
+           espnow.peerlist.list[i].peer_addr[4],
+           espnow.peerlist.list[i].peer_addr[5]);
+    M5.Lcd.println(macStr);
+    M5.Lcd.println();
+  }
+}
+
+int lastCount = 0;
 void loop() {
   M5.update();
   
+  delay(50);
+
+  if (espnow.peerlist.count > lastCount) {
+    printPeerList();  
+    lastCount = espnow.peerlist.count;
+  }
+  
   Serial.printf("peerCount: %d\r\n", espnow.peerlist.count);
   char *str = "ack";
-//  espnow.sendData(str, 4);
-//  if (espnow.peerlist.count > 0) {
-//    espnow.Ack(espnow.peerlist.list[0]);
-//  }
   
   if(M5.BtnB.isPressed()) {
     espnow.Ack(espnow.peerlist.list[0]);
